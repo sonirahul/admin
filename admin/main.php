d347e941c6eb49d3c434e92bd50efee0c92dc906
Rahul Soni <soni_rahul@live.com>
2016-07-27 11:09:11 +0530

    checking in the files

--- admin/main.php
+++ admin/main.php
@@ -1,3 +1,8 @@
+<div class="right_col" role="main">
+	<div class="row">
+		<div class="col-md-12 col-sm-12 col-xs-12">
+			<div class="x_panel">
+				<div class="x_content">
 <table width="100%" border="0">
 <tr><td>&nbsp;  </td></tr>
 <tr><td>&nbsp;  </td></tr>
@@ -9,3 +14,8 @@
 <font size="+2" color="#FF0000">Welcome to mawaredhouse Web Site administration</font>
 </td></tr>
 </table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
