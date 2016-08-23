9a50ecb69f19d6fbdc8f8032e926ae38442ab5fd
Rahul Soni <soni_rahul@live.com>
2016-08-23 18:36:37 +0530

    added a dynamic logic for team colored boxes (supports till 14)

--- index.php
+++ index.php
@@ -622,21 +622,65 @@ if($finalLang == "ar")
 		</div>
 	</div>
 
+	<?php
+			$countFromDB=count($allEmployeeTeamData);
+			$boxes = intval($countFromDB/3);
+			
+			$widthEachBox = 2;
+			$totalWidth = 12;
+			$total = ($countFromDB + $boxes) * $widthEachBox;
+			
+			$multFactor = fmod($total,$totalWidth);
+			$missingCount = 0;
+
+			if ($multFactor == 0) {
+				$multFactor = intval($total/$totalWidth);
+			}
+
+			else {
+				$multFactor = intval($total/$totalWidth) + 1;
+				$missingCount = intval((($totalWidth * $multFactor) - $total)/$widthEachBox);
+			}
+			$boxes = $missingCount + $boxes;
+
+			//echo "count from db: $countFromDB total of $multFactor rows, and $boxes coloured boxes.";
+			
+			if ($boxes == 4) {
+				$coloredBoxes=array("1:#ff5d72","10:#f1c824","14:#43c696","18:#45b1cc");
+			}
+			if ($boxes == 5) {
+				$coloredBoxes=array("2:#ff5d72","5:#f1c824","7:#43c696","15:#45b1cc","18:#ff5d72");
+			}
+			if ($boxes == 6) {
+				$coloredBoxes=array("2:#ff5d72","5:#f1c824","7:#43c696","10:#45b1cc","15:#f1c824","18:#43c696");
+			}
+			if ($boxes == 7) {
+				$coloredBoxes=array("1:#ff5d72","5:#f1c824","8:#43c696","12:#45b1cc","13:#f1c824","15:#45b1cc","17:#43c696");
+			}
+			if ($boxes == 8) {
+				$coloredBoxes=array("2:#ff5d72","4:#f1c824","6:#43c696","7:#45b1cc","9:#43c696","11:#ff5d72","14:#f1c824","16:#45b1cc");
+			}
+			
+		?>
 
 	<div class="row" style="margin:0">
 		<?php
-		$coloredBoxes=array(1,5,8,12,13,15,17);
-		$colors=array("#ff5d72","#f1c824","#43c696","#45b1cc","#f1c824","#45b1cc","#43c696");
+		
+		
+		
+		
 		$boxCount=0;
 		$totalBoxes = count($allEmployeeTeamData) + count($coloredBoxes);
 		$employeeNo=0;
+		
+		
 		for($i=1;$i<=$totalBoxes;$i++){
-			
-			if( $i == $coloredBoxes[$boxCount]) 
+			$var=explode (":",$coloredBoxes[$boxCount]);
+			if( $i == $var[0]) 
 			{
 				?>
 				<div class="col-sm-2 team-mem">
-					<div class="box" style="background-color: <?php echo $colors[$boxCount] ?>"></div>
+					<div class="box" style="background-color: <?php echo $var[1] ?>"></div>
 				</div>
 				<?php $boxCount=$boxCount+1;
 			}
