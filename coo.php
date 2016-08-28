652af88ce9165e0157586dfeb17eaa9fa88b7ccf
Rahul Soni <soni_rahul@live.com>
2016-08-28 13:17:08 -0700

    coo.php look modified.. now will try to reduce html content of that new style and map it with db

--- coo.php
+++ coo.php
@@ -18,6 +18,7 @@ $allCooData=select_query($link,$SQLforCoo,0,0);
 
 <style type="text/css">
 	body{padding-bottom:0;padding-top:50px}
+	#coo-main{background-color: #cfdfff;background-image:linear-gradient(bottom,#f6f6f6 45%,#86b0ff 100%);background-image:-moz-linear-gradient(bottom,#f6f6f6 45%,#86b0ff 100%);background-image:-webkit-linear-gradient(bottom,#f6f6f6 45%,#86b0ff 100%)}
 	ul.nav.nav-pills li:not(:last-child):after{content:"";width:50px;height:5px;background:#1a4270;display:block;position:absolute;top:50%;right:-40px}
 	ul li{overflow:initial}
 	li.active i.fa{display:none}
@@ -32,12 +33,20 @@ $allCooData=select_query($link,$SQLforCoo,0,0);
 	.nav-pills>li>a p{position:absolute;font-weight:bold;width:100%;bottom:-60px;text-align:center;text-transform: capitalize;font-size:16px;}
 
 	.tab-content{margin-top:100px}
-	.tab-content{margin-top:100px;background:#0f3d69;padding:20px;color:#fff;height:540px}
-	.tab-content .row { margin: auto;}
+	.tab-content{margin-top:100px;background:#0f3d69;padding:20px;padding:0;color:#fff;height:540px}
+	.tab-content .row { margin: auto;box-shadow: 0 0 20px #a9a9a9;}
 	//.tab-content *{color:#fff}
 
 	#coo_title{text-transform:uppercase;text-align:left;font-size:x-large;}
-
+#coo-country-name {
+    color: #333;
+    text-transform: uppercase;
+    text-align: center;
+    padding: 30px;
+    font-size: 32px;
+}
+	
+	
 	@media only screen and (min-width : 320px) and (max-width:991px){
 		#coo-main{width:100vw;padding:0}
 		.nav-pills>li{width:100%}
@@ -103,7 +112,7 @@ $allCooData=select_query($link,$SQLforCoo,0,0);
 	});
 </script>
 
-<div id="coo-main" class="section container-fluid" style="background-color: #cfdfff;">
+<div id="coo-main" class="section container-fluid">
 	<h2>Countries of Operation </h2>
 	<div id="con-content-parent">
 		<div id="coo-content-container">
@@ -131,21 +140,27 @@ $allCooData=select_query($link,$SQLforCoo,0,0);
 
 
 
-			<div class="tab-content container-fluid">
+			<div class="tab-content col-sm-8 column-center">
 				<i id="coo-content-back-btn" class="fa fa-2x fa-angle-left visible-xs visible-sm " aria-hidden="true"></i>
 				<?php 
 				for($i=0;$i<count($allCooData);$i++){?>
 					<div id="menu<?php echo $i?>" class="tab-pane fade in <?php if($i==0) echo active ?>">
 						<div class="row">
-							<div class="col-sm-6 hidden-xs" style="background:url(countries/<?php echo $allCooData[$i]["countries_photo"]?>);background-size:cover;height:500px;"></div>
-							<div id="coo-con-pic" class="col-sm-6 visible-xs"><img src="countries/<?php echo $allCooData[$i]["countries_photo"]?>" /></div>
-							<div class="col-sm-6">
-								<h2 id="coo_title">
+							<div id="coo-country-name" class="col-sm-12 "> 
 									<?php 
 									if($finalLang == "en") echo $allCooData[$i]["countries_title_en"];
 									if($finalLang == "ar") echo $allCooData[$i]["countries_title_ar"];
 									?>
-								</h2>
+							</div>
+							<div class="col-sm-12 hidden-xs" style="background:url(countries/<?php echo $allCooData[$i]["countries_photo"]?>);background-size:cover;height:500px;"></div>
+							<div id="coo-con-pic" class="col-sm-12 visible-xs"><img src="countries/<?php echo $allCooData[$i]["countries_photo"]?>" /></div>
+							<div class="col-sm-12">
+								<!--<h2 id="coo_title">
+									<?php 
+									if($finalLang == "en") echo $allCooData[$i]["countries_title_en"];
+									if($finalLang == "ar") echo $allCooData[$i]["countries_title_ar"];
+									?>
+								</h2>-->
 								<div id="coo_desc">
 									<!--<?php 
 									if($finalLang == "en") echo $allCooData[$i]["countries_desc_en"];
@@ -166,7 +181,7 @@ $allCooData=select_query($link,$SQLforCoo,0,0);
 												<div id="primary" class="content-area">
 													<main id="main" class="site-main" role="main">
 														<article id="post-98" class="entry-content-wrap post-98 post type-post status-publish format-standard has-post-thumbnail hentry category-style5">
-															<div class="post-thumbnail-wrap"><img width="1170" height="545" src="images/google-amp-fast-speed-travel-ss-1920-1170x545.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="google-amp-fast-speed-travel-ss-1920"></div>
+															<!-- <div class="post-thumbnail-wrap"><img width="1170" height="545" src="images/google-amp-fast-speed-travel-ss-1920-1170x545.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="google-amp-fast-speed-travel-ss-1920"></div>-->
 															<div class="entry-content">
 																
 																<div class="content-inner-wrap">
