16919dace87f9cacbc9949a6cd481afc80bbe2ec
Ravi Kumar Soni <ravisoni7@live.com>
2016-09-13 01:32:39 +0530

    header.php css render blocking resolved

--- header.php
+++ header.php
@@ -38,14 +38,14 @@
 	<meta content='Mawared House | Local Presence Global Support' property='og:title'>
 	<meta content="Local Presence Global Support" property='og:description'>
 
-	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/main-ce83667e.css" media="screen">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-	<link rel="stylesheet" type="text/css" href="css/content_slider_style.css">
-	<link rel="stylesheet" type="text/css" href="css/nanoscroller.css">
-	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'  media="none" onload="if(media!='all')media='all'">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" media="none" onload="if(media!='all')media='all'">
+	<link rel="stylesheet" type="text/css" href="css/main-ce83667e.css" media="screen"  media="none" onload="if(media!='all')media='all'">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css"  media="none" onload="if(media!='all')media='all'">
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css" media="none" onload="if(media!='all')media='all'">
+	<link rel="stylesheet" type="text/css" href="css/content_slider_style.css" media="none" onload="if(media!='all')media='all'">
+	<link rel="stylesheet" type="text/css" href="css/nanoscroller.css"  media="none" onload="if(media!='all')media='all'">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css"  media="none" onload="if(media!='all')media='all'">
 
 	<script src="js/main-6e0cc465.js"></script>
 	<script src="js/process-1d1f249e.js"></script>
