4306adda1ba5a0b3cd17a582f1a85e961ea75def
Rahul Soni <soni_rahul@live.com>
2016-09-15 23:32:21 +0530

    adding logic for colored boxes.

--- index.php
+++ index.php
@@ -553,20 +553,27 @@ mh-icon-colored'></i>4. <?php echo $visionName ?></h3>
 	$boxes = $missingCount + $boxes;
 
 	//echo "count from db: $countFromDB total of $multFactor rows, and $boxes coloured boxes.";
-	if ($boxes == 4) {
-		$coloredBoxes=array("1:#ff5d72","10:#f1c824","14:#43c696","18:#45b1cc");
+	if ($multFactor ==3) {
+		if ($boxes == 4) {
+			$coloredBoxes=array("1:#ff5d72","10:#f1c824","14:#43c696","18:#45b1cc");
+		}
+		if ($boxes == 5) {
+			$coloredBoxes=array("2:#ff5d72","5:#f1c824","7:#43c696","15:#45b1cc","18:#ff5d72");
+		}
+		if ($boxes == 6) {
+			$coloredBoxes=array("2:#ff5d72","5:#f1c824","7:#43c696","10:#45b1cc","15:#f1c824","18:#43c696");
+		}
+		if ($boxes == 7) {
+			$coloredBoxes=array("1:#ff5d72","5:#f1c824","8:#43c696","12:#45b1cc","13:#f1c824","15:#45b1cc","17:#43c696");
+		}
+		if ($boxes == 8) {
+			$coloredBoxes=array("2:#ff5d72","4:#f1c824","6:#43c696","7:#45b1cc","9:#43c696","11:#ff5d72","14:#f1c824","16:#45b1cc");
+		}
 	}
-	if ($boxes == 5) {
-		$coloredBoxes=array("2:#ff5d72","5:#f1c824","7:#43c696","15:#45b1cc","18:#ff5d72");
-	}
-	if ($boxes == 6) {
-		$coloredBoxes=array("2:#ff5d72","5:#f1c824","7:#43c696","10:#45b1cc","15:#f1c824","18:#43c696");
-	}
-	if ($boxes == 7) {
-		$coloredBoxes=array("1:#ff5d72","5:#f1c824","8:#43c696","12:#45b1cc","13:#f1c824","15:#45b1cc","17:#43c696");
-	}
-	if ($boxes == 8) {
-		$coloredBoxes=array("2:#ff5d72","4:#f1c824","6:#43c696","7:#45b1cc","9:#43c696","11:#ff5d72","14:#f1c824","16:#45b1cc");
+	if ($multFactor ==4) {
+		if ($boxes == 8) {
+			$coloredBoxes=array("1:#ff5d72","4:#f1c824","9:#43c696","12:#45b1cc","14:#45b1cc","17:#ff5d72","21:#f1c824","24:#43c696");
+		}
 	}
 ?>
 
