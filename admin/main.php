1301d3b68325f88ee1b47a3cd6b24f43ee8a27c7
Rahul Soni <soni_rahul@live.com>
2016-09-19 14:18:13 +0530

    fixing the admin mobile view.

--- admin/main.php
+++ admin/main.php
@@ -1,3 +1,4 @@
+<div class="admin-landing">
 <table width="100%" border="0">
 <tr><td>&nbsp;  </td></tr>
 <tr><td>&nbsp;  </td></tr>
@@ -9,3 +10,4 @@
 <font size="+2" color="#FF0000">Welcome to mawaredhouse Web Site administration</font>
 </td></tr>
 </table>
+</div>
